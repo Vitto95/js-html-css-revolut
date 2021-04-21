25285410b78d1df56fa85858817f8450ca7aec07
Vitto95 <grimaldi.vitto@gmail.com>
2021-04-21 18:03:15 +0200

    eventi over-out-click completati

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
 $(document).ready(function () {
+  console.log($(".with-dropdown").find(".active"));
+
   //events
-  $(".with-dropdown").click(function () {
-    $(this).find(".dropdown-menu").toggleClass("active");
+
+  $(".with-dropdown").mouseover(function () {
+    $(this).find(".dropdown-menu").addClass("active");
   });
 
-  /* $(".with-dropdown").click(function () {
+  $(".with-dropdown").mouseout(function () {
+    $(this).find(".dropdown-menu").removeClass("active");
+  });
+
+  $(".with-dropdown").click(function () {
+    $(this).find(".dropdown-menu").removeClass("active");
+  });
+
+  /*   $(".with-dropdown").click(function () {
     $(".with-dropdown").find(".active").removeClass("active");
     $(this).find(".dropdown-menu").addClass("active");
   }); */
